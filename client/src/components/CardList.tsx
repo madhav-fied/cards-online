42ca401d9918f3fba1b7541b31a8bddb4a6837db
madhav-fied <madhavcv2116@gmail.com>
2025-10-04 09:25:42 +0530

    fix: builds

--- client/src/components/CardList.tsx
+++ client/src/components/CardList.tsx
@@ -1,6 +1,7 @@
 
 import {Card} from "./Card";
-import type {CardProps} from "./Card"  
+import type {CardProps} from "./Card"
+import {v4 as uuidv4} from 'uuid'
 
 
 
@@ -26,7 +27,7 @@ export const CardList = ({cards, size = 3, stacked = false} : CardListProps) => 
 		<div style={cardListStyle}>
 			{
 				cards.map((card, idx) => (
-					<div key={crypto.randomUUID()} style={stacked ? {...stackedStyle, zIndex: idx,} : undefined}>
+					<div key={uuidv4()} style={stacked ? {...stackedStyle, zIndex: idx,} : undefined}>
 						<Card size={size} {...card} />
 					</div>
 				)) 
